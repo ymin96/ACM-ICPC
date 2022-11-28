88eeab3a4d938f7f13a29e73aa62d6e47df7c3c4
KimYongHwan <ymin96@naver.com>
2022-11-29 02:15:21 +0900

    BOJ 1167

--- algorithm/dfs/1167.py
+++ algorithm/dfs/1167.py
@@ -1,14 +1,32 @@
+def DFS(graph, cur, value):
+    global max_node, max_value, visit
+    if visit[cur]:
+        return
 
+    if value > max_value:
+        max_node = cur
+        max_value = value
+
+    visit[cur] = True
+    for node in graph[cur]:
+        DFS(graph, node[0], value + node[1])
 
 
 v = int(input())
 graph = [[] for _ in range(v + 1)]
-value = [[0 for j in range(v + 1)] for i in range(v + 1)]
+visit = [False for _ in range(v+1)]
 for i in range(v):
     temp = list(map(int, input().split()))
     start = temp[0]
     node = temp[1:-1]
     for j in range(0, len(node), 2):
-        graph[start].append(node[j])
-        value[start][node[j]] = node[j + 1]
-print(graph)
+        graph[start].append([node[j], node[j + 1]])
+max_node = 1
+max_value = 0
+DFS(graph, 1, 0)
+visit = [False for i in range(v+1)]
+temp = max_node
+max_node = 1
+max_value = 0
+DFS(graph, temp, 0)
+print(max_value)
