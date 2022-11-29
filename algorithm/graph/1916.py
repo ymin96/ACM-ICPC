e122fff7648438debba1dadb2bc2f694b99b5abf
Kim YongHwan <ymin96@naver.com>
2022-11-29 16:34:21 +0900

    BOJ 1916

---
+++ algorithm/graph/1916.py
@@ -0,0 +1,31 @@
+import heapq
+
+
+def dijkstra(start):
+    global distance, graph
+    que = []
+    heapq.heappush(que, (0, start))
+    distance[start] = 0
+
+    while que:
+        dist, node = heapq.heappop(que)
+        if distance[node] < dist:
+            continue
+        for next in graph[node]:
+            cost = distance[node] + next[1]
+            if cost < distance[next[0]]:
+                distance[next[0]] = cost
+                heapq.heappush(que, (cost, next[0]))
+
+
+n = int(input())
+m = int(input())
+INF = 9999999999999
+graph = [[] for i in range(n + 1)]
+distance = [INF] * (n + 1)
+for i in range(m):
+    start, end, dist = map(int, input().split())
+    graph[start].append([end, dist])
+start, end = map(int, input().split())
+dijkstra(start)
+print(distance[end])
