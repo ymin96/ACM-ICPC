a879fe6a874d68ff12eea3e89d987dbd99bec621
KimYongHwan <ymin96@naver.com>
2022-12-04 01:34:41 +0900

    BOJ 1504

--- algorithm/graph/1504.py
+++ algorithm/graph/1504.py
@@ -1,11 +1,9 @@
 import heapq
 
-
-def dijkstra(start):
-    global distance, graph
+def dijkstra(start, distance):
+    global graph
     que = []
-    heapq.heappush(que, (0, start))
-    distance[start] = 0
+    heapq.heappush(que, (distance[start], start))
 
     while que:
         dist, node = heapq.heappop(que)
@@ -25,6 +23,28 @@ distance = [INF] * (n + 1)
 for i in range(m):
     start, end, dist = map(int, input().split())
     graph[start].append([end, dist])
+    graph[end].append([start, dist])
 start, end = map(int, input().split())
-dijkstra(start)
-print(distance[end])
+distance[1] = 0
+dijkstra(1, distance)
+
+dist_a1 = [INF] * (n+1)
+dist_a1[start] = distance[start]
+dijkstra(start, dist_a1)
+dist_a2 = [INF] * (n+1)
+dist_a2[end] = dist_a1[end]
+dijkstra(end, dist_a2)
+min_a = dist_a2[-1]
+
+dist_b1 = [INF] * (n+1)
+dist_b1[end] = distance[end]
+dijkstra(end, dist_b1)
+dist_b2 = [INF] * (n+1)
+dist_b2[start] = dist_b1[start]
+dijkstra(start, dist_b2)
+min_b = dist_b2[-1]
+
+if min_a >= INF and min_b >= INF:
+    print(-1)
+else:
+    print(min(min_a, min_b))
